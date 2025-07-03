a0441ff404a7c0f515c2aa9dc6e57f93d133adba
Jonathan Murphy <jonathan@jmurphy.me>
2025-07-03 15:29:54 -0400

    stole some things from ublue to get a working desktop

--- build_files/10w_gnome_setup.sh
+++ build_files/10w_gnome_setup.sh
@@ -2,4 +2,22 @@ debug "Inside gnome setup"
 
 
 dnf group install -y gnome-desktop gnome-games gnome-software-development
+dnf install -y "mesa-libGLU" \
+    "nautilus-gsconnect" \
+    "gnome-shell-extension-appindicator" \
+    "gnome-shell-extension-blur-my-shell" \
+    "gnome-shell-extension-caffeine" \
+    "gnome-shell-extension-dash-to-dock" \
+    "gnome-shell-extension-gsconnect" \
+    "gnome-shell-extension-logo-menu" \
+    "gnome-shell-extension-search-light" \
+    "gnome-shell-extension-tailscale-gnome-qs" \
+    "gnome-tweaks" \
+    "pulseaudio-utils" \
+    "adw-gtk3-theme" 
+
 systemctl set-default graphical.target
+
+# Install flaptak, stolen from ublue
+flatpak remote-add --system --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+systemctl disable flatpak-add-fedora-repos.service
